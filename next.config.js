a32eea1661ec232934d9c7d1658751e86e569416
tom <83908974+goat6@users.noreply.github.com>
2023-05-30 15:46:00 +0100

    changed next.config to accomodate

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-// next.config.js
 module.exports = {
   images: {
     domains: ['cdn.discordapp.com'],
   },
-}
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
